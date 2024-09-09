3bf43f16a0b65762d5764cbfc91e42631339b9de
odai.abughaith <odai.abughaith@ese.gov.ae>
2024-09-09 12:00:08 +0400

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    eslint: {
-        // Set this to true to ignore ESLint errors during builds
-        ignoreDuringBuilds: true,
-      },
-      typescript: {
-        // Set this to true to ignore TypeScript errors
-        ignoreBuildErrors: true,
-      },
-   
+  reactStrictMode: false,
+  eslint: {
+    // Set this to true to ignore ESLint errors during builds
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Set this to true to ignore TypeScript errors
+    ignoreBuildErrors: true,
+  },
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL, // Example environment variable
+    LoginURL: process.env.LoginURL,
+  },
 };
 
 export default nextConfig;
